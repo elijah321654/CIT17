45872eab56712df663aa778810c49489f0a93edf
elijah321654 <56668089+elijah321654@users.noreply.github.com>
2022-09-20 20:15:48 +0800

    Add files via upload

---
+++ Exercise3.php
@@ -0,0 +1,35 @@
+<html>	
+<body>
+<?php 
+        print " Exercise 3 ";
+        $num = 8;
+
+
+        $val1 = "Value is now "; $num;
+        $val2 = "Add 2. Value is now "; $add = $num +2;
+        $val3 = "Subtract 4. Value is now "; $Subtract = $add - 4;
+        $val4 = "Multiply by 5. Value is now "; $Multiply = $Subtract * 5;
+        $val5 = "Divide by 3. Value is now "; $Divide = $Multiply / 3;
+        $val6 = "Increment value by one. Value is now "; $Increment = $Divide +1;
+        $val7 = "Decrement value by one. Value is now "; $Decrement = $Increment -1;
+        
+
+        echo $val1;
+        echo $num . "<br>";
+        echo $val2;
+        echo $add. "<br>";
+        echo $val3;
+        echo $Subtract. "<br>";
+        echo $val4;
+        echo $Multiply. "<br>";
+        echo $val5;
+        echo $Divide. "<br>";
+        echo $val6;
+        echo $Increment. "<br>";
+        echo $val7;
+        echo $Decrement. "<br>";
+
+    
+    ?>
+	</body>
+</html>
