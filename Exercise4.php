45c388f5b28a87404e398fd1f0200463188d531a
elijah321654 <56668089+elijah321654@users.noreply.github.com>
2022-09-20 20:18:24 +0800

    Add files via upload

---
+++ Exercise4.php
@@ -0,0 +1,24 @@
+<html>	
+<body>
+<?php 
+        <?php print " Exercise 4 "; 
+
+        $name = "Harry";
+        $num = 28;
+        $a = NULL;
+        
+        
+        var_dump($name);
+        echo "<br>";
+        echo $name;
+        echo "<br>";
+        var_dump($num);
+        echo "<br>";
+        var_dump($a);
+
+
+    
+    ?>
+
+	</body>
+</html>
---
+++ Exercise5.php
@@ -0,0 +1,14 @@
+<html>	
+<body>
+<<?php 
+
+    print " PHP Exercise 5 ";
+
+        $around = "around";
+
+        echo 'What goes '  . $around  . ', comes '   . $around . '.' . "<br>" . "<br>"; 
+    
+    ?>
+
+	</body>
+</html>
