d4bd523c533ac96007fdc27a71b389c02a7d8277
elijah321654 <56668089+elijah321654@users.noreply.github.com>
2022-09-20 19:58:47 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,23 @@
+<html>	
+<body>
+<?php 
+    print " Exercise 1 >";
+
+    $first = "Twinkle, Twinkle little star";
+
+    $text = "Twinkle";
+    $text1 = "\tstar";
+    
+    $text2 = "\tRinkle";
+    $text3 = "dimple";
+
+    echo $first . "<br>" . "<br>";
+    echo $text;
+    echo $text2. "<br>" ;
+    echo $text3;
+    echo $text1. "<br>" . "<br>" ;
+
+
+    php?>
+	</body>
+</html>
